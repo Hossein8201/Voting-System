cb5353608f657806575d0dd2c9344062f720aae0
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 02:47:14 +0330

    feat: create Person class and add filds on it.

---
+++ Person.java
@@ -0,0 +1,17 @@
+/**
+ * <h1>Create a person</h1>
+ * In this class we create a person by Firstname and Lastname to can participate in Vote system. <p>
+ * @author Hossein Tatar - 40133014
+ * @version 1.0
+ * @since 2024-10-28
+ */
+public class Person {
+    /*
+    First we define the variables as follows:
+    */
+    /** <b>firstName</b> is a String type. */
+    private String firstName;
+    /** <b>lastName</b> is a String type. */
+    private String lastName;
+
+}
