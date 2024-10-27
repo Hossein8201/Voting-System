33a848073b5f5c9dca0f6d241b787d05a9ea3760
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 03:03:31 +0330

    feat: add getter and setter methods to person class

--- Person.java
+++ Person.java
@@ -1,6 +1,7 @@
 /**
  * <h1>Create a person</h1>
- * In this class we create a person by Firstname and Lastname to can participate in Vote system. <p>
+ * In this class we create a person by Firstname and Lastname to can participate in Vote system.
+ * <p>
  * @author Hossein Tatar - 40133014
  * @version 1.0
  * @since 2024-10-28
@@ -24,5 +25,24 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    /*
+    Next we need to use getter and setter to apply the changes for private variables
+    */
+    /** Getter method to get the value of firstName parameter.
+     * @return a String type that is firstName of person. */
+    public String getFirstName() {return firstName;}
+    /** Getter method to get the value of lastName parameter.
+     * @return a String type that is lastName of person. */
+    public String getLastName() {return lastName;}
+     /*
+    Now we identify the Setter methods.
+    */
+    /** Setter method to change the value of firstName.
+     * @param firstName is a String type of input value.
+     * @return nothing */
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    /** Setter method to change the value of lastName.
+     * @param lastName is a Stirng type of input value.
+     * @return nothing */
+    public void setLastName(String lastName) {this.lastName = lastName;}
 }
