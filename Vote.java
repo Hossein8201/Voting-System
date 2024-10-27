cd7f99036b1c44b91315f5d690b5220ed43400cb
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 03:23:13 +0330

    feat: create Vote class

---
+++ Vote.java
@@ -0,0 +1,10 @@
+/**
+ * <h1>Create a Vote</h1>
+ * In this class, We create a vote with information of voter and date.
+ * <p>
+ * @author Hossein Tatar - 40133014
+ * @version 1.0
+ * @since 2024-10-28
+ */
+public class Vote {
+}
