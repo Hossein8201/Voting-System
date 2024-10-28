183fabafee8c599302aab859bfd4efa47a543b33
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 12:44:51 +0330

    feat: create voting system class

---
+++ VotingSystem.java
@@ -0,0 +1,9 @@
+/**
+ * <h1>Handling the voting process</h1>
+ * This class is responsible for managing all changes and processes of voting.
+ * @author Hossein Tatar - 40133014
+ * @version 1.0
+ * @since 2024-10-28
+ */
+public class VotingSystem {
+}
