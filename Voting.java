5b048b94f67baba82bd5f3b967c31c30fc55c945
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 09:52:52 +0330

    feat: create Voting class

---
+++ Voting.java
@@ -0,0 +1,10 @@
+/**
+ * <h1>Voting properties</h1>
+ * In this class, We define many options and types for voting.
+ * <p>
+ * @author Hossein Tatar - 40133014
+ * @version 1.0
+ * @since 2024-10-28
+ */
+public class Voting {
+}
