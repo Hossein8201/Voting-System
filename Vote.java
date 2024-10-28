787a196e67a68ffafb5d881785a026ed1680c4ab
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 04:10:50 +0330

    feat: override equals and hashcode methods and add to vote class

--- Vote.java
+++ Vote.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * <h1>Create a Vote</h1>
  * In this class, We create a vote with information of voter and date.
@@ -29,6 +30,7 @@ public class Vote {
     }
      /*
     Next we need to use getter methods to access the value of private variables.
+    We don't need to setter methods because we don't want to change the value of fields.
     */
     /** Getter method to get the value of voter parameter.
      * @return an object of Person class that is information of person. */
@@ -36,4 +38,26 @@ public class Vote {
     /** Getter method to get the value of date parameter.
      * @return a String type that is date of vote. */
     public String getDate() {return date;}
+    /**
+     * <h3>Override of Equals method</h3>
+     * This method check the votes together to be sure no person <b>don't voted twice</b>.
+     * @param o is an object.
+     * @return is a boolean type that show us the vote is repetitive or not.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote = (Vote) o;
+        return Objects.equals(voter, vote.voter) && Objects.equals(date, vote.date);
+    }
+    /**
+     * <h3>Override of hashcode method </h3>
+     * This method transform the fields to hash codes to make the <b>accessibility faster</b>.
+     * @return is a int type that is hash code of the fields.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(voter, date);
+    }
 }
