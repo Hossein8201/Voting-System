8d379f5fe45eb28f394a3538f08c30b20ea0094d
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 12:53:16 +0330

    feat: add fields and constructor to voting system class

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * <h1>Handling the voting process</h1>
  * This class is responsible for managing all changes and processes of voting.
@@ -6,4 +7,17 @@
  * @since 2024-10-28
  */
 public class VotingSystem {
+    /*
+    First we define the fields and constructor as follows:
+    */
+    /** <b>votingList</b> is an ArrayList of Voting class objects that store the voting. */
+    private ArrayList<Voting> votingList;
+    /**
+     * <h3>Constructor method of votingSystem</h3>
+     * This method creates an object of VotingSystem class and initialized that, We dont need to input.
+     * @return nothing
+     */
+    public VotingSystem() {
+        this.votingList = new ArrayList<Voting>();
+    }
 }
