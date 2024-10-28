f7ad22b07a0b76150c58388ddd7596f6f3e808f6
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 03:38:10 +0330

    feat: add constructor to vote class

--- Vote.java
+++ Vote.java
@@ -16,5 +16,16 @@ public class Vote {
     /** <b>date</b> is a String type.<p>
      * <b>note:</b> we don't need to change the value of that, Thus we define it <i>final</i>. */
     private final String date;
+    /**
+     * <h3>Constructor of votes</h3>
+     * This method creates an object of Vote class and initialized that.
+     * @param voter is an object of Person class.
+     * @param date is a String type of input value.
+     * @return nothing
+     */
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
 
 }
