55ea36d6c0fc250baac08a71b020a1b104a462ac
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-28 14:55:29 +0330

    feat: add methods of feature VotingSystem class

--- VotingSystem.java
+++ VotingSystem.java
@@ -36,4 +36,38 @@ public class VotingSystem {
         }
         votingList.add(voting);
     }
+    /**
+     * <h3>Find a voting</h3>
+     * According to the argument, it finds the desired voting index in the votingList.
+     * @param index is an int type that is argument
+     * @return is an object of Voting class that argument refer to.
+     */
+    public Voting getVoting(int index) {return votingList.get(index);}
+    /** Getter method to get the value of votingList.
+     * @return is an ArrayList that every member are an object of Voting class. */
+    public ArrayList<Voting> getVotingList() {return votingList;}
+    /**
+     * <h3>Print the result</h3>
+     * It prints the final voting result with given index number.
+     * @param index is an int value that show us what argument must print.
+     * @return nothing
+     */
+    public void printResults(int index) { votingList.get(index).printResults();}
+    /**
+     * <h3>Print the list of voters</h3>
+     * This method prints the list of voters for voting with given argument.
+     * @param index is an int value that show us what argument must print its voters.
+     * @return nothing
+     */
+    public void printVoters(int index) { votingList.get(index).printVoters();}
+    /**
+     * <h3>Print the information of voting</h3>
+     * This method prints the question and the options of each voting.
+     * @param index is an int value that show us what argument must print its information.
+     * @return nothing
+     */
+    public void printVoting(int index) {
+        System.out.println("The question of voting is : "+ votingList.get(index).getQuestion() + " and it choices: ");
+        System.out.println(votingList.get(index).getChoices());
+    }
 }
